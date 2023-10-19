b575d92329cdfd5c181cd25ad2ba8f9ff40e0aa5
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-10-19 14:54:45 -0300

    Update DepositoM.java

--- src/DepositoM.java
+++ src/DepositoM.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 
 class DepositoM {
+
+    //Se crea una lista que almacena Moneda
     private ArrayList<Moneda> ver;
 
     public DepositoM() {
         ver = new ArrayList();
     }
 
+    //metodo para agregar  Moneda al arreglo
     public void addMoneda() {
         Moneda mon = new Moneda100();
         ver.add(mon);
     }
-
+    //metodo para obtenera la informacio del primera Monedas y sacarlo de arreglo
     public Moneda getMoneda() {
         if (ver.size() == 0) {
             return null;
