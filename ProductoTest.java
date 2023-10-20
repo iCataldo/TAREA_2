b44375fd981fd983bd634d60b2c3febb543f96c3
Ignacio Cataldo <146041107+iCataldo@users.noreply.github.com>
2023-10-19 22:18:11 -0300

    Update ProductoTest.java

--- ProductoTest.java
+++ ProductoTest.java
@@ -6,16 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductoTest {
 
-    @BeforeEach
-    void setUp() {
+    @org.junit.jupiter.api.BeforeAll
+    static void setUp() {
+        System.out.println("@BeforeAll executed");
     }
 
-    @AfterEach
-    void tearDown() {
+    @org.junit.jupiter.api.BeforeEach
+    void setUpThis() {
+        System.out.println("@BeforeEach executed");
+    }
+    
+    @org.junit.jupiter.api.AfterEach
+    void tearDownThis() {
+        System.out.println("@AfterEach executed");
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    static void tearDown() {
+        System.out.println("@AfterAll executed");
     }
 
     @Test
     void getSerie() {
+        System.out.println("=====TEST getSerie() EXECUTED=====");
         assertAll(()->assertEquals(1, new Snickers(1).getSerie()),
                 ()->assertEquals(2, new CocaCola(2).getSerie()),
                 ()->assertEquals(3, new Super8(3).getSerie()),
@@ -23,6 +36,7 @@ class ProductoTest {
     }
     @Test
     void beber() {
+        System.out.println("=====TEST beber() EXECUTED=====");
         assertAll(()->assertEquals("snickers", new Snickers(1).beber()),
                 ()->assertEquals("cocacola", new CocaCola(2).beber()),
                 ()->assertEquals("sprite", new Sprite(3).beber()),
