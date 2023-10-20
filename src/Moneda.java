3c37e4b2ec5cc138789a7600699acdaf67b89f49
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-10-20 16:02:50 -0300

    Update Moneda.java
    documentación de la Moneda

--- src/Moneda.java
+++ src/Moneda.java
@@ -4,6 +4,7 @@ abstract class Moneda{
     public Moneda getSerie(){
         return this;
     }
+    //@return el valor de la Moneda
     public abstract int getValor();
 }
 //Se crea un tipo de Moneda que devuelva un valor de 100
@@ -11,7 +12,7 @@ class Moneda100 extends Moneda {
     public Moneda100() {
         super();
     }
-
+     //@return la de Moneda100 vale 100 
     public int getValor() {
         return 100;
     }
@@ -21,6 +22,7 @@ class Moneda500 extends Moneda{
     public Moneda500(){
         super();
     }
+     //@return la de Moneda100 vale 100 
     public int getValor() {
         return 500;
     }
@@ -30,6 +32,7 @@ class Moneda1000 extends Moneda{
     public Moneda1000(){
         super();
     }
+     //@return la de Moneda100 vale 100 
     public int getValor() {
         return 1000;
     }
@@ -39,6 +42,7 @@ class Moneda1500 extends Moneda{
     public Moneda1500(){
         super();
     }
+     //@return la de Moneda100 vale 100 
     public int getValor() {
         return 1500;
     }
